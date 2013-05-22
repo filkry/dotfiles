873b03ed858696a40c4ed21e7c7927bbf6d1a35c
Fil Krynicki <filipkrynicki@gmail.com>
2013-05-22 18:02:24 -0400

    added ruby

--- arch/install.sh
+++ arch/install.sh
@@ -1,1 +1,2 @@
-sudo pacman -S --needed base base-devel xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font libreoffice
+sudo pacman -Sy
+sudo pacman -S --needed base base-devel xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font libreoffice ruby
