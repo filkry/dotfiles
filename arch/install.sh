81a7787310858bbd453d8568933ef744aa0b281c
Fil Krynicki <filipkrynicki@gmail.com>
2013-05-22 17:51:59 -0400

    added base and base-devel to arch setup

--- arch/install.sh
+++ arch/install.sh
@@ -1,1 +1,1 @@
-sudo pacman -S --needed xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font
+sudo pacman -S --needed base base-devel xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font libreoffice
