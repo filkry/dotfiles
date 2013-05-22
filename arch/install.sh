aacc69439415f95fbd5a288b82a872cf18723d45
Fil Krynicki <filipkrynicki@gmail.com>
2013-05-22 17:42:48 -0400

    made arch install runnable and changed so existing packages are not reinstalled

--- arch/install.sh
+++ arch/install.sh
@@ -1,1 +1,1 @@
-pacman -S xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-fon
+sudo pacman -S --needed xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font
