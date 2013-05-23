34aabe308a7e3ffdae4b20ffa96417aab9a06aea
Filip Krynicki <filipkrynicki@gmail.com>
2013-05-23 12:18:30 -0400

    removed need to manually touch up pacman commands, added missing deps

--- arch/install.sh
+++ arch/install.sh
@@ -1,2 +1,2 @@
 sudo pacman -Sy
-sudo pacman -S --needed base base-devel xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font ruby encfs
+sudo pacman -S --noconfirm --needed base base-devel xorg-server xorg-server-utils xorg-xinit xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font ruby encfs openssh qt4
