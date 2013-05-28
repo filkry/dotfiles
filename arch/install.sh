77ffdcaeec57268224594f293783bcaab0456e57
Filip Krynicki <filipkrynicki@gmail.com>
2013-05-28 12:25:17 -0400

    printer stuff

--- arch/install.sh
+++ arch/install.sh
@@ -1,6 +1,9 @@
 sudo pacman -Sy
 sudo pacman -S --noconfirm --needed base base-devel xorg-server xorg-server-utils xorg-xinit xmonad xmonad-contrib xfce4 chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font ruby encfs openssh qt4 evince
 
+# just install a stupid amount of printer drivers
+sudo pacman -S --noconfirm --needed libcups cups gutenprint hplip splix cups-pdf
+
 # Install yaourt
 mkdir -p ~/downloads
 cd ~/downloads
