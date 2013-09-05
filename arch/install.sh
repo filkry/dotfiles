b8e5514f97bdd681571ffdad6a6dc421060342d1
Filip Krynicki <filipkrynicki@gmail.com>
2013-09-05 09:42:14 -0400

    xmobar

--- arch/install.sh
+++ arch/install.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy
-sudo pacman -S --noconfirm --needed base base-devel xorg-server xorg-server-utils xorg-xinit xmonad xmonad-contrib chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font ruby encfs openssh qt4 evince rsync xmobar trayer alsa-utils volumeicon
+sudo pacman -S --noconfirm --needed base base-devel xorg-server xorg-server-utils xorg-xinit xmonad xmonad-contrib chromium firefox dmenu wget vim rxvt-unicode unzip virtualbox terminus-font ruby encfs openssh qt4 evince rsync xmobar trayer alsa-utils volumeicon xmobar
 
 # just install a stupid amount of printer drivers
 sudo pacman -S --noconfirm --needed libcups cups gutenprint hplip splix cups-pdf
