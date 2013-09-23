d6079b307c1db0a72ceb366d42fa32fbbfa7dc2a
Filip Krynicki <filipkrynicki@gmail.com>
2013-09-23 09:45:27 -0400

    pulseaudio install and startup

--- arch/install_aur.sh
+++ arch/install_aur.sh
@@ -1,3 +1,5 @@
-yaourt -S --aur --needed --noconfirm google-chrome vagrant zotero spideroak dropbox gitflow-git sublime-text pasystray-git pulseaudio_ctl
+yaourt -S --aur --needed --noconfirm google-chrome vagrant zotero spideroak dropbox sublime-text pasystray-git pulseaudio_ctl
+
+yaourt -S --aur --needed --noconfirm gitflow-git
 
 vagrant plugin install vagrant-aws
