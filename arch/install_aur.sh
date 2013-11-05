81329942c3bb3b5c31eb3595ca17861ffec6afda
Filip Krynicki <filipkrynicki@gmail.com>
2013-11-05 10:48:54 -0500

    new pulseaudio-ctl package

--- arch/install_aur.sh
+++ arch/install_aur.sh
@@ -1,4 +1,4 @@
-yaourt -S --aur --needed --noconfirm google-chrome vagrant zotero spideroak dropbox sublime-text pasystray-git pulseaudio_ctl
+yaourt -S --aur --needed --noconfirm google-chrome vagrant zotero spideroak dropbox sublime-text pasystray-git pulseaudio-ctl
 
 yaourt -S --aur --needed --noconfirm gitflow-git
 
