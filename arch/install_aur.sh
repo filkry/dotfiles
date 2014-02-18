61cb1679c5d4f759d95d656041d04dce6f82554e
Filip Krynicki <filipkrynicki@gmail.com>
2014-02-18 09:23:33 -0500

    done

--- arch/install_aur.sh
+++ arch/install_aur.sh
@@ -1,3 +1,9 @@
-yaourt -S --aur --needed --noconfirm google-chrome spideroak dropbox sublime-text pasystray-git pulseaudio-ctl gtk-theme-iris-dark-git
+yaourt -S --aur --needed --noconfirm google-chrome 
+yaourt -S --aur --needed --noconfirm spideroak 
+yaourt -S --aur --needed --noconfirm dropbox 
+yaourt -S --aur --needed --noconfirm sublime-text 
+yaourt -S --aur --needed --noconfirm pasystray-git 
+yaourt -S --aur --needed --noconfirm pulseaudio-ctl 
+yaourt -S --aur --needed --noconfirm gtk-theme-iris-dark-git
 
 yaourt -S --aur --needed --noconfirm gitflow-git
